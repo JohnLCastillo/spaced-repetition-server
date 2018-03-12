6155234cdb6ffdf6c72a9cb46e3d3520863e96c8
John Castillo <JohnluisCastillo@gmail.com>
2018-03-12 16:40:14 -0400

    env

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://Jc:Asdfasdf1@ds139267.mlab.com:39267/spaced-rep',
+        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
     TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
