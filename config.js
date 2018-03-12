532882df014ff41ae2bbfde2a5124c45b6c2186e
John Castillo <JohnluisCastillo@gmail.com>
2018-03-12 14:07:38 -0400

    auth working

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://jc:Asdfasdf1@ds139267.mlab.com:39267/spaced-rep',
+        process.env.DATABASE_URL || 'mongodb://Jc:Asdfasdf1@ds139267.mlab.com:39267/spaced-rep',
     TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
     JWT_SECRET :
-     process.env.JWT_SECRET,
+     process.env.JWT_SECRET || 'smh',
     JWT_EXPIRY :
      process.env.JWT_EXPIRY || '7d'
     // DATABASE_URL:
